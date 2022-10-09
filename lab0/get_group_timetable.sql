d2b018a78876912b1781ccedb6e003209041ead8
mu2so4 <b4.orangutang@gmail.com>
2022-10-09 17:01:50 +0700

    Add group timetable query

---
+++ lab0/get_group_timetable.sql
@@ -0,0 +1,24 @@
+SELECT
+	"Weekday"."День недели",
+	"Timetable"."Время начала",
+	"Timetable"."Время конца",
+	"Аудитория",
+	"Subject"."Полное название" AS "Дисциплина",
+	"Class_format"."Название" AS "Тип пары",
+	"Teacher"."Фамилия"+ SPACE(1) + "Teacher"."Имя" + SPACE(1) +
+		IFNULL("Teacher"."Отчество", '') AS Преподаватель
+FROM "Class"
+	INNER JOIN "Weekday"
+		ON "Class"."День недели" = "Weekday"."ID"
+	INNER JOIN "Timetable"
+		ON "Class"."Номер пары" = "Timetable"."Порядок"
+	INNER JOIN "Discipline"
+		ON "Class"."Предмет" = "Discipline"."ID"
+	INNER JOIN "Subject"
+		ON "Discipline"."Содержание" = "Subject"."ID"
+	INNER JOIN "Teacher"
+		ON "Class"."Преподаватель" = "Teacher"."ID"
+		
+	INNER JOIN "Class_format"
+		ON "Class"."Тип пары" = "Class_format"."ID"
+WHERE "Группа" = :Enter_Group
